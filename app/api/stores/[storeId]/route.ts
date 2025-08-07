3ad7c7bac1d85940fa29a6b7b0aff5e72c88916a
melky <melky@melkys-MacBook-Pro.local>
2025-08-07 20:55:07 +0700

    deploy

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -4,11 +4,12 @@ import { NextResponse } from "next/server"
 
 export async function PATCH (
     req: Request,
-    {params}: {params: {storeId: string}}
+    {params}: {params: Promise<{storeId: string}>}
 ) {
     try {
         const { userId } = await auth()
         const body = await req.json();
+        const {storeId} = await params
 
         const { name } = body;
 
@@ -18,13 +19,13 @@ export async function PATCH (
         if (!name) {
             return new NextResponse("Harus menginput nama", { status: 400});
         }
-        if (!params.storeId) {
+        if (!storeId) {
             return new NextResponse("Store id dibutuhkan", { status: 400 });
         }
 
         const store = await db.store.updateMany({
             where: {
-                id: params.storeId,
+                id: storeId,
                 userId
             },
             data: {
