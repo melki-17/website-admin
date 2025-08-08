45fc52c6c0eb650d5f5a9f5bbbe0e97367456027
melky <melky@melkys-MacBook-Pro.local>
2025-08-08 15:50:08 +0700

    Edit Harga

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -32,7 +32,7 @@ interface ProductFormProps {
 const formSchema = z.object({
     name: z.string().min(1),
     images: z.object({url: z.string()}).array(),
-    price: z.coerce.number().min(1, "Price must be greater than 0"),
+    price: z.coerce.number().min(1),
     categoryId: z.string().min(1),
     isFeatured: z.boolean().default(false).optional(),
     isArchived: z.boolean().default(false).optional(),
